1ea015c90ace49f017958eb1a70fe03ae3d0906a
yjxiong <xy012@ie.cuhk.edu.hk>
2016-03-05 23:15:36 +0800

    add scale and bias layers

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -803,6 +803,102 @@ protected:
     Blob<Dtype> sum_multiplier_, norm_, squared_;
 };
 
+
+template <typename Dtype>
+class ScaleLayer: public Layer<Dtype> {
+public:
+    explicit ScaleLayer(const LayerParameter& param)
+            : Layer<Dtype>(param) {}
+    virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+                            const vector<Blob<Dtype>*>& top);
+    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+                         const vector<Blob<Dtype>*>& top);
+
+    virtual inline const char* type() const { return "Scale"; }
+    // Scale
+    virtual inline int MinBottomBlobs() const { return 1; }
+    virtual inline int MaxBottomBlobs() const { return 2; }
+    virtual inline int ExactNumTopBlobs() const { return 1; }
+
+protected:
+    /**
+     * In the below shape specifications, @f$ i @f$ denotes the value of the
+     * `axis` field given by `this->layer_param_.scale_param().axis()`, after
+     * canonicalization (i.e., conversion from negative to positive index,
+     * if applicable).
+     *
+     * @param bottom input Blob vector (length 2)
+     *   -# @f$ (d_0 \times ... \times
+     *           d_i \times ... \times d_j \times ... \times d_n) @f$
+     *      the first factor @f$ x @f$
+     *   -# @f$ (d_i \times ... \times d_j) @f$
+     *      the second factor @f$ y @f$
+     * @param top output Blob vector (length 1)
+     *   -# @f$ (d_0 \times ... \times
+     *           d_i \times ... \times d_j \times ... \times d_n) @f$
+     *      the product @f$ z = x y @f$ computed after "broadcasting" y.
+     *      Equivalent to tiling @f$ y @f$ to have the same shape as @f$ x @f$,
+     *      then computing the elementwise product.
+     */
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+                             const vector<Blob<Dtype>*>& top);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                             const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+    shared_ptr<Layer<Dtype> > bias_layer_;
+    vector<Blob<Dtype>*> bias_bottom_vec_;
+    vector<bool> bias_propagate_down_;
+    int bias_param_id_;
+
+    Blob<Dtype> sum_multiplier_;
+    Blob<Dtype> sum_result_;
+    Blob<Dtype> temp_;
+    int axis_;
+    int outer_dim_, scale_dim_, inner_dim_;
+};
+
+/**
+ * @brief Computes a sum of two input Blobs, with the shape of the
+ *        latter Blob "broadcast" to match the shape of the former.
+ *        Equivalent to tiling the latter Blob, then computing the elementwise
+ *        sum.
+ *
+ * The second input may be omitted, in which case it's learned as a parameter
+ * of the layer.
+ */
+template <typename Dtype>
+class BiasLayer : public Layer<Dtype> {
+public:
+    explicit BiasLayer(const LayerParameter& param)
+            : Layer<Dtype>(param) {}
+    virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+                            const vector<Blob<Dtype>*>& top);
+    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+                         const vector<Blob<Dtype>*>& top);
+
+    virtual inline const char* type() const { return "Bias"; }
+    virtual inline int MinBottomBlobs() const { return 1; }
+    virtual inline int MaxBottomBlobs() const { return 2; }
+    virtual inline int ExactNumTopBlobs() const { return 1; }
+
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+                             const vector<Blob<Dtype>*>& top);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                             const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                              const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+private:
+    Blob<Dtype> bias_multiplier_;
+    int outer_dim_, bias_dim_, inner_dim_, dim_;
+};
+
 }  // namespace caffe
 
 #endif  // CAFFE_COMMON_LAYERS_HPP_
