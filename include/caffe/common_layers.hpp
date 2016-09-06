095cdea9c4437e261ea875de2ece2ed202f95355
Cysu <st.cysu@gmail.com>
2016-09-06 22:05:53 +0800

    GPU impl of synchronized bn layer

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -783,7 +783,7 @@ class BNLayer : public Layer<Dtype> {
 };
 
 
-#if defined(USE_CUDNN) 
+#if defined(USE_CUDNN)
 #if CUDNN_VERSION_MIN(5, 0, 0)
 /**
  * @brief cuDNN implementation of BNLayer.
@@ -820,6 +820,44 @@ class CuDNNBNLayer : public BNLayer<Dtype> {
   Blob<Dtype> save_inv_variance_;
 };
 #endif
+#endif
+
+#ifdef USE_MPI
+template <typename Dtype>
+class SyncBNLayer : public Layer<Dtype> {
+ public:
+  explicit SyncBNLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "SyncBN"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  Dtype bn_momentum_;
+  Dtype bn_eps_;
+
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+
+  Blob<Dtype> mean_buffer_;
+  Blob<Dtype> var_buffer_;
+};
 #endif
 
 /**
